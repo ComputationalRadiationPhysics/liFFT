68cd2d3189f6740c1eccbbf0947b8e87a067df1f
Flamefire <alex@grundis.de>
2015-07-01 00:12:57 +0200

    Make generateData more generic

--- generateData.hpp
+++ generateData.hpp
@@ -1,13 +1,34 @@
 #pragma once
 
-template< typename T, class Generator>
-void generateData(Volume<T>& data, const Generator& generator){
-    for(size_t z=0; z<data.zDim(); ++z){
-        for(size_t y=0; y<data.yDim(); ++y){
-            for(size_t x=0; x<data.xDim(); ++x)
-                data(x,y,z) = generator(x,y,z);
-        }
+#include "foobar/traits/NumDims.hpp"
+#include "foobar/traits/DefaultAccessor.hpp"
+#include "foobar/policies/Copy.hpp"
+#include "foobar/policies/GetExtents.hpp"
+#include "foobar/mem/DataContainer.hpp"
+
+struct GeneratorAccessor
+{
+    template< class T_Idx, class T_Data >
+    std::result_of_t<std::remove_pointer_t<T_Data>(size_t, size_t, size_t)>
+    operator()(T_Idx&& idx, const T_Data& data)
+    {
+        static constexpr unsigned numDims = foobar::traits::NumDims<T_Idx>::value;
+        size_t z = (numDims < 3) ? 0 : idx[numDims - 3];
+        size_t y = (numDims < 2) ? 0 : idx[numDims - 2];
+        size_t x = idx[numDims - 1];
+        return (*data)(x, y, z);
     }
+};
+
+template< typename T, class Generator, class T_Accessor = foobar::traits::DefaultAccessor_t<T> >
+void generateData(T& data, const Generator& generator, const T_Accessor& acc = T_Accessor()){
+    static constexpr unsigned numDims = foobar::traits::NumDims<T>::value;
+    foobar::mem::DataContainer< numDims, const Generator*, GeneratorAccessor, false > genContainer;
+    genContainer.data = &generator;
+    foobar::policies::GetExtents<T> extents(data);
+    for(unsigned i=0; i<numDims; i++)
+        genContainer.extents[i] = extents[i];
+    foobar::policies::makeCopy(typename decltype(genContainer)::Accessor(), acc)(genContainer, data);
 }
 
 template<typename T>
