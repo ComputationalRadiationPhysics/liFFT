2fe34304155305d378083fb48b7a91cb147f0246
Flamefire <alex@grundis.de>
2015-07-03 18:11:18 +0200

    Add tiff testcase

--- include/libTiff/traitsAndPolicies.hpp
+++ include/libTiff/traitsAndPolicies.hpp
@@ -3,15 +3,24 @@
 #include "libTiff/libTiff.hpp"
 #include "foobar/traits/NumDims.hpp"
 #include "foobar/traits/DefaultAccessor.hpp"
+#include "foobar/traits/IsComplex.hpp"
+#include "foobar/traits/IsStrided.hpp"
 #include "foobar/policies/VolumeAccessor.hpp"
 #include "foobar/policies/GetExtents.hpp"
 #include "foobar/types/Vec.hpp"
+#include "foobar/util.hpp"
 
 namespace foobar {
 namespace traits {
 
     template< libTiff::ImageFormat T_imgFormat, class T_Allocator >
     struct NumDims< libTiff::Image< T_imgFormat, T_Allocator > >: std::integral_constant<unsigned, 2>{};
+
+    template< libTiff::ImageFormat T_imgFormat, class T_Allocator >
+    struct IsComplex< libTiff::Image< T_imgFormat, T_Allocator > >: BoolConst<false>{};
+
+    template< libTiff::ImageFormat T_imgFormat, class T_Allocator >
+    struct IsStrided< libTiff::Image< T_imgFormat, T_Allocator > >: BoolConst<false>{};
 
     template< libTiff::ImageFormat T_imgFormat, class T_Allocator >
     struct DefaultAccessor< libTiff::Image< T_imgFormat, T_Allocator > >
--- test/testFile.cpp
+++ test/testFile.cpp
@@ -14,9 +14,7 @@ namespace foobarTest {
     void testFile( T_File& file )
     {
         using FFTResult_t = foobar::mem::ComplexContainer<2, TestPrecision>;
-        FFTResult_t fftResult;
-        fftResult.extents = foobar::types::Idx2D(file.getExtents()[1], file.getExtents()[0]/2+1);
-        fftResult.allocData();
+        FFTResult_t fftResult(foobar::types::Idx2D(file.getExtents()[1], file.getExtents()[0]/2+1));
         using FFT_Type = foobar::FFT_2D_R2C<TestPrecision>;
         auto input = FFT_Type::wrapFFT_Input(file);
         auto output = FFT_Type::wrapFFT_Output(fftResult);
@@ -29,7 +27,25 @@ namespace foobarTest {
         if(!res.first)
             std::cerr << "Error for R2C with file input: " << res.second << std::endl;
         else
-            std::cout << "C2C with file input passed" << std::endl;
+            std::cout << "R2C with file input passed" << std::endl;
+    }
+
+    void testTiffFile(const std::string& filePath)
+    {
+        libTiff::FloatImage<> img(filePath, false);
+        using FFT_Type = foobar::FFT_2D_R2C_F;
+        auto input = FFT_Type::wrapFFT_Input(img);
+        auto output = FFT_Type::getNewFFT_Output(input);
+        auto fft = foobar::makeFFT< TestLibrary, false >(input, output);
+        img.load();
+        foobar::policies::copy(img, baseR2CInput);
+        fft(input, output);
+        execBaseR2C();
+        auto res = compare(baseR2COutput, output);
+        if(!res.first)
+            std::cerr << "Error in TIFF test: " << res.second << std::endl;
+        else
+            std::cout << "TIFF test passed" << std::endl;
     }
 
     void testFile()
@@ -42,6 +58,7 @@ namespace foobarTest {
             >;
         FileType myFile("rect.tif");
         testFile(myFile);
+        testTiffFile("input1.tif");
     }
 
 }  // namespace foobarTest
