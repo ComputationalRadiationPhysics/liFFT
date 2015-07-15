079ec95be1c5b5832ab4e13fa4f2306d182a79e6
Alexander Grund <a.grund@hzdr.de>
2015-07-15 14:35:26 +0200

    Allow 3D FFT of tiff images

--- fftTiffImg/main.cpp
+++ fftTiffImg/main.cpp
@@ -4,7 +4,7 @@
 #include "libTiff/image.hpp"
 #include "libTiff/traitsAndPolicies.hpp"
 #include "foobar/FFT.hpp"
-#ifdef WITH_CUDA
+#if defined(WITH_CUDA) and false // Cuda memory is not enough
 #include "foobar/libraries/cuFFT/cuFFT.hpp"
 using FFT_LIB = foobar::libraries::cuFFT::CuFFT<>;
 #else
@@ -22,7 +22,8 @@ using std::string;
 using std::cout;
 using std::cerr;
 
-string replace(string str, const string& from, const string& to) {
+string
+replace(string str, const string& from, const string& to) {
     size_t start_pos = str.find(from);
     if(start_pos == string::npos)
         return str;
@@ -36,7 +37,8 @@ void showHelp()
     cout << desc << std::endl;
 }
 
-void do2D_FFT(const string& inFilePath, const string& outFilePath)
+void
+do2D_FFT(const string& inFilePath, const string& outFilePath)
 {
     using namespace foobar;
     using FFT = FFT_2D_R2C_F;
@@ -54,35 +56,115 @@ void do2D_FFT(const string& inFilePath, const string& outFilePath)
     outImg.save();
 }
 
-int main(int argc, char** argv)
+/**
+ * Copies data from an image to a 3D container using the slice at the specified z-offset
+ * @param img  Image to copy
+ * @param data Datacontainer to copy to
+ * @param zDim z-Index
+ * @param acc  Accessor to use to access the data
+ */
+template< class T_Img, class T_Data, class T_Acc = foobar::traits::DefaultAccessor_t<T_Data> >
+void
+copy2Data(const T_Img& img, T_Data& data, unsigned zDim, const T_Acc& acc = T_Acc())
 {
-    unsigned firstIdx, lastIdx;
+    foobar::types::Vec<3> idx(zDim, 0u, 0u);
+    for(; idx[1] < img.getHeight(); ++idx[1])
+        for(; idx[2] < img.getWidth(); ++idx[2])
+            acc(idx, data) = img(idx[2], idx[1]);
+}
+
+/**
+ * Copies data from a 3D container to an image using the slice at the specified z-offset
+ * @param data Datacontainer to copy from
+ * @param img  Image to copy to
+ * @param zDim z-Index
+ * @param acc  Accessor to use to access the data
+ */
+template< class T_Img, class T_Data, class T_Acc = foobar::traits::DefaultAccessor_t<T_Data> >
+void
+copy2Img(const T_Data& data, T_Img& img, unsigned zDim,  const T_Acc& acc = T_Acc())
+{
+    foobar::types::Vec<3> idx(zDim, 0u, 0u);
+    for(; idx[1] < img.getHeight(); ++idx[1])
+        for(; idx[2] < img.getWidth(); ++idx[2])
+            img(idx[2], idx[1]) = acc(idx, data);
+}
+
+string
+getFilledNumber(unsigned num, unsigned minSize, char filler)
+{
+    string s(std::to_string(num));
+    while(s.size()<minSize)
+        s = filler + s;
+    return s;
+}
+
+int
+main(int argc, char** argv)
+{
+    unsigned firstIdx, lastIdx, minSize;
+    char filler;
     string inFilePath, outFilePath;
     desc.add_options()
-        ("help", "Show help message")
+        ("help,h", "Show help message")
         ("input-file,i", po::value<string>(&inFilePath), "Input file to use, can contain %i as a placeholder for 3D FFTs")
         ("output-file,o", po::value<string>(&outFilePath)->default_value("output.tif"), "Output file to write to")
         ("firstIdx", po::value<unsigned>(&firstIdx)->default_value(0), "first index to use")
         ("lastIdx", po::value<unsigned>(&lastIdx)->default_value(0), "last index to use")
+        ("minSize,s", po::value<unsigned>(&minSize)->default_value(0), "Minimum size of the string replaced for %i")
+        ("fillChar,f", po::value<char>(&filler)->default_value('0'), "Char used to fill the string to the minimum size")
     ;
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
 
-    if (vm.count("help") || !vm.count("input-file") || inFilePath.empty() || outFilePath.empty()) {
+    if (vm.count("help") || !vm.count("input-file") || inFilePath.empty() || outFilePath.empty())
+    {
         showHelp();
         return 1;
     }
 
-    if(firstIdx > lastIdx) {
+    if(firstIdx > lastIdx)
+    {
         std::cerr << "FirstIdx must not be greater then lastIdx" << std::endl;
         return 1;
     }
 
     if(firstIdx == lastIdx || inFilePath.find("%i") == string::npos)
     {
-        inFilePath = replace(inFilePath, "%i", std::to_string(firstIdx));
+        // Only 1 image --> 2D FFT
+        inFilePath = replace(inFilePath, "%i", getFilledNumber(firstIdx, minSize, filler));
         do2D_FFT(inFilePath, outFilePath);
+        return 0;
     }
+
+    // Multiple images --> 3D FFT
+    // Assume all images have the same size --> load the first one to get extents and create FFT Data
+    string curFilePath = replace(inFilePath, "%i", getFilledNumber(firstIdx, minSize, filler));
+    libTiff::FloatImage<> img(curFilePath);
+    using FFT = foobar::FFT_3D_R2C_F;
+    auto input = FFT::wrapFFT_Input(
+                    foobar::mem::RealContainer<3, float>(
+                            foobar::types::Vec<3>(lastIdx-firstIdx+1, img.getHeight(), img.getWidth())
+                    )
+                 );
+    auto output = FFT::getNewFFT_Output(input);
+    auto fft = foobar::makeFFT<FFT_LIB, false>(input, output);
+    // Now copy all the data into one memory region
+    copy2Data(img, input, 0);
+    for(unsigned i=firstIdx+1; i<=lastIdx; ++i)
+    {
+        curFilePath = replace(inFilePath, "%i", getFilledNumber(i, minSize, filler));
+        img.open(curFilePath);
+        copy2Data(img, input, i-firstIdx);
+    }
+    // Do the FFT
+    fft(input, output);
+    // Copy the intensities to the img and save it
+    auto acc = foobar::accessors::makeTransformAccessorFor(foobar::policies::CalcIntensityFunc(), output);
+    copy2Img(output, img, 0, acc);
+    img.saveTo(outFilePath);
+
+    return 0;
 }
