62b39cb8b97e198aebea911f213b70ccfed70f9e
Alexander Grund <a.grund@hzdr.de>
2015-07-29 15:07:32 +0200

    Fix param check

--- fftTiffImg/main.cpp
+++ fftTiffImg/main.cpp
@@ -92,7 +92,7 @@ main(int argc, char** argv)
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
 
-    if (vm.count("help") || !vm.count("input-file") || inFilePath.empty() || outFilePath.empty())
+    if (vm.count("help") || !vm.count("inputFile") || inFilePath.empty() || outFilePath.empty())
     {
         showHelp();
         return 1;
