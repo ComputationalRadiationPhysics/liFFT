08658561175341eb355bd9d96918f612b295d671
Alexander Grund <a.grund@hzdr.de>
2015-09-08 13:09:11 +0200

    Add versions

---
+++ include/foobar/foobar.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#define FOOBAR_VERSION_MAJOR 0
+#define FOOBAR_VERSION_MINOR 1
+#define FOOBAR_VERSION_PATCH 0
---
+++ include/libTiff/libTiff.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "image.hpp"
+
+#define LIBTIFF_VERSION_MAJOR 0
+#define LIBTIFF_VERSION_MINOR 1
+#define LIBTIFF_VERSION_PATCH 0
