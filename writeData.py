a1438e996530954bebdb2a6c981806f5142b58a9
Flamefire <git@grundis.de>
2016-04-01 11:05:06 +0200

    Remove files with unclear source

--- writeData.py
+++ writeData.py
@@ -1,12 +1,37 @@
-from helpModule import *
-from formFactor import *
 from matplotlib import cm
+from matplotlib import pyplot as plt
+from matplotlib.colors import LogNorm
 import numpy as np
-import sys, getopt
+import sys
+import argparse
 
 delta_x = 1.
 delta_y = 1.
 
+def drawGraphic(data, figure, xLabel = None, yLabel = None, title = None, logPlot = True, extent = None):
+
+    if logPlot:
+        norm = LogNorm()
+    else:
+        norm = None
+        
+    subplot = figure.add_subplot(111)
+    pic = subplot.imshow(data, norm=norm, aspect="auto", cmap=cm.spectral, extent=extent)
+
+    if xLabel:
+        plt.xlabel(xLabel, fontsize=20)
+    if yLabel:
+        plt.ylabel(yLabel, fontsize=20)
+
+    plt.xticks(size=16)
+    plt.yticks(size=16)
+
+    if title:
+        plt.title(title, fontsize=24)
+    CB = figure.colorbar(pic)
+    for t in CB.ax.get_yticklabels():
+        t.set_fontsize(20)
+            
 def writeSpectral(data, outFp):
   Nx = len(data[0])
   Ny = len(data)
@@ -15,15 +40,13 @@ def writeSpectral(data, outFp):
   sizeX = 1024*20/Nx
   sizeY = 1024*16/Ny
   FIG = plt.figure(0, figsize=(sizeX, sizeY))
-  create_2Dmap(data,
-               figure=FIG,
-               aspect='auto',
-               cmap=cm.spectral,
-               logScale=logOutput,
-               cbar=True,
-               xlabel=r"$k_x$",
-               ylabel=r"$k_y$",
-               extent=(-kx_max, kx_max, -ky_max, ky_max))
+  drawGraphic(data,
+               FIG,
+               "$k_x$",
+               "$k_y$",
+               "Spectral colored data (arbitrary units)",
+               True,
+               (-kx_max, kx_max, -ky_max, ky_max))
   FIG.savefig(outFp, format='pdf')
   plt.close(FIG)
   
@@ -33,14 +56,11 @@ def writeNormal(data, outFp):
   sizeX = 1024*20/Nx
   sizeY = 1024*16/Ny
   FIG = plt.figure(0, figsize=(sizeX, sizeY))
-  create_2Dmap(data,
-               figure=FIG,
-               aspect='auto',
-               cmap=cm.spectral,
-               logScale=logOutput,
-               cbar=True,
-               xlabel=r"xPos in mm",
-               ylabel=r"yPos in mm")
+  drawGraphic(data,
+               FIG,
+               r"xPos",
+               r"yPos",
+               "Spectral colored data (arbitrary units)")
   FIG.savefig(outFp, format='pdf')
   plt.close(FIG)
 
@@ -53,27 +73,15 @@ def loadAndWriteData(inFp, outFp, isSpectral):
 
   
 def main(name, argv):
-  inputfile = ''
-  outputfile = 'output.pdf'
-  isSpectral = False
-  try:
-    opts, args = getopt.getopt(argv,"hsi:o:",["isSpectral","ifile=","ofile="])
-  except getopt.GetoptError:
-    print(name+' -i <inputfile> -o <outputfile>')
-    sys.exit(2)
-  for opt, arg in opts:
-    if opt == '-h':
-       print(name+' -i <inputfile> -o <outputfile>')
-       sys.exit()
-    elif opt in ("-i", "--ifile"):
-       inputfile = arg
-    elif opt in ("-o", "--ofile"):
-       outputfile = arg
-    elif opt in ("-s", "--isSpectral"):
-       isSpectral = True
+  parser = argparse.ArgumentParser(description="Create PDF image from text input", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument('-i', '--ifile', required=True, help="Path to inputfile")
+  parser.add_argument('-o', '--ofile', default="output.pdf", help="Path to outputfile")
+  parser.add_argument('-s', '--isSpectral', action="store_true", help="Use spectral scaled output")
+  options = parser.parse_args(argv)
   
-  print('Loading "' + inputfile + '" to "' + outputfile + '"')
-  loadAndWriteData(inputfile, outputfile, isSpectral)
+  print('Loading "' + options.ifile + '" to "' + options.ofile + '"')
+  loadAndWriteData(options.ifile, options.ofile, options.isSpectral)
        
 if __name__ == "__main__":
    main(sys.argv[0], sys.argv[1:])
+
